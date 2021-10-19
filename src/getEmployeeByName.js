b2b55a030b3e77a071a36edbd3125f5a70a67516
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-10-19 16:40:26 -0300

    Terceiro Requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.filter((funcionario) => {
+    if (funcionario.firstName === employeeName || funcionario.lastName === employeeName) {
+      return funcionario;
+    }
+    return undefined;
+  });
 }
-
 module.exports = getEmployeeByName;
