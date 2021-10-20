40e38b11099a70ce32cd07ad21e5a36a8757dcdf
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-10-19 22:03:59 -0300

    Quinto Requisito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const manager = employees.reduce((acc, curr) => acc.concat(curr.managers), []);
+  return manager.some((funcionario) => funcionario === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const subordinados = employees.filter((funcionar) => funcionar.managers.includes(managerId));
+    return subordinados.map((sub) => `${sub.firstName} ${sub.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
 module.exports = { isManager, getRelatedEmployees };
