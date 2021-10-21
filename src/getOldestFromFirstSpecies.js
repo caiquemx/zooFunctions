ce34d16d76ae16538f65dbbae94f778395ed98ca
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-10-21 16:24:47 -0300

    Nono Requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,44 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function filtro(ids) {
+  const idx = [];
+  if (Array.isArray(ids) === false) {
+    idx.push(ids);
+  }
+  idx.push(...ids);
+  const arrBase = employees.filter((ele, index) => idx[index] === ele.id)
+    .map((car) => car.responsibleFor[0]);
+  const resBase = [];
+  arrBase.forEach((ele) => {
+    resBase.push(species.find((ani) => ani.id === ele).residents);
+  });
+  return resBase;
+}
+function ordenar(ids) {
+  const ordenado = [];
+  filtro(ids).forEach((ele) => {
+    ele.sort((a, b) => {
+      if (a.age > b.age) {
+        return -1;
+      }
+      if (a.age < b.age) {
+        return 1;
+      }
+      return 0;
+    });
+    ordenado.push(ele[0]);
+  });
+  return ordenado;
 }
 
+function getOldestFromFirstSpecies(ids) {
+  const final = [];
+  ordenar(ids).reduce((acc, curr) => {
+    acc.push(`${curr.name}`, `${curr.sex}`, curr.age);
+    final.push(acc);
+    return [];
+  }, []);
+  return final;
+}
 module.exports = getOldestFromFirstSpecies;
