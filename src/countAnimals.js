877d5111501570be45b083665141b8f4ddc0821c
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-10-20 23:59:07 -0300

    Quinto Requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,48 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function getSpecies(animal) {
+  const especieFitlrada = species.filter((spec) => spec.name === animal.specie);
+  return especieFitlrada.flatMap((elem) => elem.residents);
 }
 
+function countMale(animal) {
+  let countMale1 = 0;
+  getSpecies(animal).forEach((caract) => {
+    if (caract.sex === 'male') {
+      countMale1 += 1;
+    }
+  });
+  return countMale1;
+}
+
+function countFemale(animal) {
+  let countFemale1 = 0;
+  getSpecies(animal).forEach((caract) => {
+    if (caract.sex === 'female') {
+      countFemale1 += 1;
+    }
+  });
+  return countFemale1;
+}
+
+function countAnimals(animal) {
+  if (!animal) {
+    const Obj1 = {};
+    species.forEach((specie) => {
+      Obj1[specie.name] = specie.residents.length;
+    });
+    return Obj1;
+  }
+  if (!animal.sex) {
+    return species.find((specie) => specie.name === Object.values((animal))[0]).residents.length;
+  }
+  if (animal.sex === 'male') {
+    return countMale(animal);
+  }
+  if (animal.sex === 'female') {
+    return countFemale(animal);
+  }
+}
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 module.exports = countAnimals;
