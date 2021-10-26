2d83b249bb8973ee54ce26567a6557fce4652f4e
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-10-26 11:54:49 -0300

    Referencias

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,5 +1,7 @@
 const data = require('../data/zoo_data');
 
+// lógica inspirada: https://github.com/tryber/sd-015-b-project-zoo-functions/pull/1/files#diff-5dcbb838d9c588f0256440b8e5125b4f2f3f525681b36a7c2a2c7d01805dc57e
+
 function getOldestFromFirstSpecies(id) {
   const fEmployee = data.employees.find((employee) => employee.id === id).responsibleFor[0];
   const fAnimal = data.species.find((specie) => specie.id === fEmployee).residents;
