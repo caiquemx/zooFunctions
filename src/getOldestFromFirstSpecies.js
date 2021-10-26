a47d274ec59adc087a3dfa2047b57c7ecc3bc911
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-10-26 11:41:10 -0300

    Nono Requisito refeito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,44 +1,14 @@
-const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function filtro(ids) {
-  const idx = [];
-  if (Array.isArray(ids) === false) {
-    idx.push(ids);
-  }
-  idx.push(...ids);
-  const arrBase = employees.filter((ele, index) => idx[index] === ele.id)
-    .map((car) => car.responsibleFor[0]);
-  const resBase = [];
-  arrBase.forEach((ele) => {
-    resBase.push(species.find((ani) => ani.id === ele).residents);
-  });
-  return resBase;
-}
-function ordenar(ids) {
-  const ordenado = [];
-  filtro(ids).forEach((ele) => {
-    ele.sort((a, b) => {
-      if (a.age > b.age) {
-        return -1;
-      }
-      if (a.age < b.age) {
-        return 1;
-      }
-      return 0;
-    });
-    ordenado.push(ele[0]);
-  });
-  return ordenado;
+function getOldestFromFirstSpecies(id) {
+  const fEmployee = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const fAnimal = data.species.find((specie) => specie.id === fEmployee).residents;
+  return Object.values(fAnimal.reduce((acc, curr) => {
+    if (acc.age < curr.age) {
+      return curr;
+    }
+    return acc;
+  }));
 }
 
-function getOldestFromFirstSpecies(ids) {
-  const final = [];
-  ordenar(ids).reduce((acc, curr) => {
-    acc.push(`${curr.name}`, `${curr.sex}`, curr.age);
-    final.push(acc);
-    return [];
-  }, []);
-  return final;
-}
 module.exports = getOldestFromFirstSpecies;
