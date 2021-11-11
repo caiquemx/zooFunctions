a5c1113d79dbe744ad8b5a5cdb217bca3b8c3100
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-11-11 16:42:20 -0300

    Setimo Requisito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,46 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+// lógica inspirada: https://github.com/tryber/sd-015-b-project-zoo-functions/blob/3d970a74e783b56fbab548bfadb20757789e9dfd/src/getAnimalMap.js
+
+function searchDefault() {
+  const regions = {};
+  species.forEach((specie) => {
+    const { location, name } = specie;
+    if (!regions[location]) {
+      regions[location] = [];
+    }
+    regions[location].push(name);
+  });
+  return regions;
 }
 
+function getAnimalNames(paramSorted, paramSex) {
+  const regions = {};
+  species.forEach((specie) => {
+    const { location, name } = specie;
+    if (!regions[location]) {
+      regions[location] = [];
+    }
+    let residents = [...specie.residents];
+    if (paramSex) {
+      residents = residents.filter(({ sex }) => sex === paramSex)
+        .map((resident) => resident.name);
+    }
+    if (paramSorted) {
+      residents.sort();
+    }
+    regions[location].push({ [name]: residents });
+  });
+
+  return regions;
+}
+
+function getAnimalMap(options = {}) {
+  const { includeNames, sorted, sex } = options;
+
+  if (includeNames) {
+    return getAnimalNames(sorted, sex);
+  }
+  return searchDefault();
+}
 module.exports = getAnimalMap;
