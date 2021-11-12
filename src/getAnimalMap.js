96f61e7af6df010955372ce0296559b8774bd34e
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-11-12 12:40:31 -0300

    Decimo Requisito e Setimo corrigido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,6 +1,7 @@
-const { species } = require('../data/zoo_data');
 
 // lógica inspirada: https://github.com/tryber/sd-015-b-project-zoo-functions/blob/3d970a74e783b56fbab548bfadb20757789e9dfd/src/getAnimalMap.js
+
+const { species } = require('../data/zoo_data');
 
 function searchDefault() {
   const regions = {};
@@ -23,24 +24,23 @@ function getAnimalNames(paramSorted, paramSex) {
     }
     let residents = [...specie.residents];
     if (paramSex) {
-      residents = residents.filter(({ sex }) => sex === paramSex)
-        .map((resident) => resident.name);
+      residents = residents.filter(({ sex }) => sex === paramSex);
     }
+    residents = residents.map((resident) => resident.name);
     if (paramSorted) {
       residents.sort();
     }
     regions[location].push({ [name]: residents });
   });
-
   return regions;
 }
 
 function getAnimalMap(options = {}) {
   const { includeNames, sorted, sex } = options;
-
   if (includeNames) {
     return getAnimalNames(sorted, sex);
   }
   return searchDefault();
 }
+
 module.exports = getAnimalMap;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,59 @@
-const data = require('../data/zoo_data');
+// lógica inspirada https://github.com/tryber/sd-015-b-project-zoo-functions/blob/3d970a74e783b56fbab548bfadb20757789e9dfd/src/getEmployeesCoverage.js
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = require('../data/zoo_data');
+
+function findLocationOfAnimal(animals) {
+  const locations = animals.map((animal) => species.find(({ name }) => name === animal).location);
+  return locations;
+}
+
+function convertAnimalIdInName(speciesIds) {
+  const speciesNames = speciesIds.map((specie) => species.find(({ id }) => id === specie).name);
+  return speciesNames;
+}
+
+function idOrName(identifier) {
+  const { id, name } = identifier;
+  let employee;
+  if (id) {
+    employee = employees.find((people) => people.id === id);
+  } else {
+    employee = employees.find(({ firstName, lastName }) => firstName === name || lastName === name);
+  }
+  return employee;
+}
+
+function findEmployeeByIdOrName(identifierType) {
+  const employee = idOrName(identifierType);
+  if (employee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const { firstName, lastName, responsibleFor, id } = employee;
+  const speciesName = convertAnimalIdInName(responsibleFor);
+  const fullName = `${firstName} ${lastName}`;
+  const locations = findLocationOfAnimal(speciesName);
+  const infosEmployee = {
+    id,
+    fullName,
+    species: speciesName,
+    locations,
+  };
+  return infosEmployee;
+}
+
+function coverageOfAllEmployees() {
+  const employeesIds = employees.map(({ id }) => id);
+  const infosOfAllEmployees = [];
+  employeesIds.forEach((employee) => {
+    infosOfAllEmployees.push(findEmployeeByIdOrName({ id: employee }));
+  });
+  return infosOfAllEmployees;
+}
+function getEmployeesCoverage(identifier) {
+  if (identifier) {
+    return findEmployeeByIdOrName(identifier);
+  }
+  return coverageOfAllEmployees();
 }
 
 module.exports = getEmployeesCoverage;
